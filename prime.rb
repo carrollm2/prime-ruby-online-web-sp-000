17ad0a9924e8654a87bc8133de40103f32a170b9
Michael Carroll <carrollm2@gmail.com>
2019-10-18 00:04:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  end
+
+
+  divisor = ((number.to_f ** 0.5).ceil) - 1
+
+  if divisor == 1
+    return false
+  end
+
+
+  while divisor > 1
+
+    quotient = number / divisor
+    if quotient.integer?
+      return false
+    end
+    divisor -= 1
+  end
+  return true
+
+end
